1e07718adbd94bb21851b7947d579a1e66549c18
Matías Báez <matiasbaez2512@gmail.com>
2023-10-15 02:28:42 -0400

    Calculator design

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,5 @@
+
+export 'package:calculator/widgets/calc_button.dart';
+export 'package:calculator/widgets/line_separator.dart';
+export 'package:calculator/widgets/main_result.dart';
+export 'package:calculator/widgets/sub_result.dart';
